9a1d327796178fbd8e89136c34649026cd9bac91
Ivan <granic.ivan.01@gmail.com>
2025-10-20 16:41:15 +0200

    implement employee and vacation modules

--- company-portal/src/app/app.routes.ts
+++ company-portal/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+    path: 'test-wo-jwt',
+    loadComponent: () =>
+      import('./modules/testing/components/test-wo-jwt/test-wo-jwt')
+        .then(m => m.TestWoJwt)
+  },
+        {
+    path: 'user-dashboard',
+    loadComponent: () =>
+      import('./modules/shared/components/user-dashboard/user-dashboard')
+        .then(m => m.UserDashboard)
+  }
+];
---
+++ company-portal/src/app/modules/testing/components/test-wo-jwt/test-wo-jwt.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-test-wo-jwt',
+  imports: [CommonModule],
+  templateUrl: './test-wo-jwt.html',
+  styleUrls: ['./test-wo-jwt.scss']
+})
+export class TestWoJwt {
+
+  // testResponse: String | undefined;
+
+  testResponse: any;
+  
+
+  constructor(private http: HttpClient) {}
+
+//localhost:8081/auth/testNoJwt
+
+  testWOjwt() {
+    this.http.get('http://localhost:8081/auth/testNoJwtJson', { responseType: 'text' }
+    ).subscribe(response => {
+      // this.testResponse = response.toString();
+      this.testResponse = response;
+      // console.log(this.testResponse);
+      
+    });
+  }
+
+}
