59f32fba6303f0e4ecfcb40d17f597c534b230ce
GranicIvan <granic.ivan.01@gmail.com>
2025-10-31 00:21:50 +0100

    Styling 2

--- company-portal/src/app/modules/shared/components/user-dashboard/user-dashboard.ts
+++ company-portal/src/app/modules/shared/components/user-dashboard/user-dashboard.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../../service/account-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './user-dashboard.html',
   styleUrls: ['./user-dashboard.scss']
 })
